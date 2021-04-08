9c48d624ffe73b4c4eb6f045e10d57f29f7378c1
GaziAdib <greatadib82@gmail.com>
2021-04-08 21:42:08 +0600

    first commit

---
+++ news_api/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import requests
+API_KEY = 'd0b69496c18e463f888a273cb521ea9f'
+
+
+# Create your views here.
+
+
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'news_api/home.html', context)
+
+
